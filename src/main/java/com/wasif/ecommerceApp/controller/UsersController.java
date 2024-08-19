0506ef3464feb5c5875102c6ad2c4c80a68e30ff
wasif22001 <wasif22001@gmail.com>
2024-08-19 23:40:22 +0500

    Created an endpoint to get users by id.

--- src/main/java/com/wasif/ecommerceApp/controller/UsersController.java
+++ src/main/java/com/wasif/ecommerceApp/controller/UsersController.java
@@ -4,6 +4,7 @@ import com.wasif.ecommerceApp.service.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +21,11 @@ public class UsersController {
     ResponseEntity<List<String>> getUsers() {
         var users = usersService.getUsers();
         return ResponseEntity.ok(users);
+    }
+
+    @GetMapping("/{userId}")
+    ResponseEntity<String> getUserById(@PathVariable("userId") int userId) {
+        var user = usersService.getUserById(userId);
+        return ResponseEntity.ok(user);
     }
 }
